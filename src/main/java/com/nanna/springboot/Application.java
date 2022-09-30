240826ec62026fc69850f6345daabf84f4ad56d0
nanna <navkishoredas@gmail.com>
2022-09-30 23:06:19 +0530

    update

--- src/main/java/com/nanna/springboot/Application.java
+++ src/main/java/com/nanna/springboot/Application.java
@@ -10,6 +10,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         HelloController nannaDash = new HelloController();
+        //hello world
 
 
 
