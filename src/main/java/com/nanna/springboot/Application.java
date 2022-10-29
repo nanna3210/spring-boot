7508d976ea825916673c69f70467e15aa4ec5f11
nanna3210 <navkishoredas@gmail.com>
2022-10-29 23:19:13 +0530

    start

--- src/main/java/com/nanna/springboot/Application.java
+++ src/main/java/com/nanna/springboot/Application.java
@@ -1,6 +1,6 @@
 package com.nanna.springboot;
 
-import com.nanna.springboot.controller.HelloController;
+//import com.nanna.springboot.controller.HelloController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        HelloController nannaDash = new HelloController();
+//        HelloController nannaDash = new HelloController();
 
 
 
